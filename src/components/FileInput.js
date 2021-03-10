2de50cbf7b052b6d109eafc05ce91d93b1e9fac9
Heeyoung Song <hsong8179@gmail.com>
2021-03-10 10:05:30 -0500

    update File input

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,4 +1,7 @@
+import { Paper } from '@material-ui/core'
+import { CloudUpload } from '@material-ui/icons'
 import React from 'react'
+import Dropzone from 'react-dropzone'
 import { Controller } from 'react-hook-form'
 
 
@@ -9,6 +12,18 @@ export default function FileInput({control, name}) {
             control={control}
             name={name}
             defaultValue={[]}
+            render={(onChange, onBlur, value) => (<>
+             <Dropzone onDrop={onChange}>
+                 {({ getRootProps, getInputProps}) => (
+                     <Paper variant="outlined" {...getRootProps()}>
+                         <CloudUpload />
+                         <input {...getInputProps()} name={name} onBlur={onBlur} />
+                         <p>Drag 'n' drop files here, or click to select files</p>
+                     </Paper>
+                 )}
+
+             </Dropzone>
+            </>)}
             >
 
         </Controller>
